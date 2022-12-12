a8a1423abe079a01ad894e4d875b3a63f8642267
Elena <myEmail@example.com>
2022-12-12 14:01:41 +0300

    Сейф

---
+++ WpfApp7/информация о туристе.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Логика взаимодействия для информация_о_туристе.xaml
+    /// </summary>
+    public partial class информация_о_туристе : Window
+    {
+        public информация_о_туристе()
+        {
+            InitializeComponent();
+        }
+    }
+}
